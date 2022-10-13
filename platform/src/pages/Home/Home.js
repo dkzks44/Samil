07227b5bd4d6601b9d86e23ff23338fcad9cdf40
dkzks44 <dkzks44@users.noreply.github.com>
2022-10-13 12:57:53 +0900

    수정중

--- platform/src/pages/Home/Home.js
+++ platform/src/pages/Home/Home.js
@@ -3,21 +3,23 @@ import "./Home.scss"
 
 function App() {
   return (
-    <div>
-      <div className="navbar">
-        <div className="logo">A-Z incubating</div>
-        <button>로그인</button>
-      </div>
-      <div className="main_page">
-        <span>전문가를 전문가답게 : A-Z 인큐베이팅</span>
-        <div className="info_box">
-          <div>업종</div>
-          <div>예산<input/></div>
-          <div>사업장소재지</div>
+    <>
+    <div className="wrap">
+        <div className="navbar">
+          <div className="logo">A-Z incubating</div>
+          <button>로그인</button>
         </div>
-        <button>진단받기</button>
+        <div className="main_page">
+          <span>전문가를 전문가답게 : A-Z 인큐베이팅</span>
+          <div className="info_box">
+            <div>업종</div>
+            <div>예산<input/></div>
+            <div>사업장소재지</div>
+          </div>
+          <button>진단받기</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
