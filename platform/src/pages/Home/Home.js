ffb76cfc3708aebffc9db4f5f54ac47412e53193
dkzks44 <dkzks44@users.noreply.github.com>
2022-10-17 21:58:21 +0900

    Main:UI프레임 작업

--- platform/src/pages/Home/Home.js
+++ platform/src/pages/Home/Home.js
@@ -12,11 +12,20 @@ function App() {
         <div className="main_page">
           <span>전문가를 전문가답게 : A-Z 인큐베이팅</span>
           <div className="info_box">
-            <div>업종</div>
-            <div>예산<input/></div>
-            <div>사업장소재지</div>
+            <div className="job">
+              <div>업종</div>
+              <div className="job_select">업종을 선택해 주세요.</div>
+            </div>
+            <div className="budget">
+              <div>예산</div>
+              <div className="budget_input"><input/>원</div>
+            </div>
+            <div className="workspace">
+              <div>사업장소재지</div>
+              <div className="workspace_select">사업장 위치를 입력해 주세요.</div>
+            </div>
           </div>
-          <button>진단받기!!</button>
+          <button>진단받기</button>
         </div>
       </div>
     </>
