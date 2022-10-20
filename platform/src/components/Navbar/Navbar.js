e5a4c190f703f052dac25e5a6a5d3d68807fa253
dkzks44 <dkzks44@users.noreply.github.com>
2022-10-20 23:29:01 +0900

    Navbar: setup navigate

--- platform/src/components/Navbar/Navbar.js
+++ platform/src/components/Navbar/Navbar.js
@@ -3,15 +3,22 @@ import { useNavigate } from 'react-router';
 
 const Navbar = () => {
   let navigate = useNavigate();
+  const moveToHome = () => {
+    navigate('/')
+  }
+
   const moveToLogIn = () => {
     navigate('/login');
+  }
+  const moveToIntroduce = () => {
+    navigate('/introduce')
   }
 
     return (
         <div className="navbar">
-          <div className="logo">A-Z incubating</div>
+          <div className="logo" onClick={moveToHome}>A-Z incubating</div>
           <div className="menu">
-            <div>서비스소개</div>
+            <div onClick={moveToIntroduce}>서비스소개</div>
             <button onClick={moveToLogIn}>로그인</button>
           </div>
         </div>
--- platform/src/pages/Introduce/Introduce.js
+++ platform/src/pages/Introduce/Introduce.js
@@ -5,7 +5,10 @@ const Introduce = () => {
     return (
         <>
             <Navbar/>
-            <div className="test">Hello!</div>
+            <div>삼일기업의 마케팅 서비스</div>
+            <div>저희는 다릅니다. 차별점 예시</div>
+            <div>저희는 이런 프로세스로 진행합니다</div>
+            <div>삼일에서 함께하세요</div>
         </>
     );
 }
